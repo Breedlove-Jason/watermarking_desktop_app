115fb3a3d08a0d7c860bcde20b49954a26579898
Jason Breedlove <jbreedlove@jasonbreedlove.dev>
2023-06-25 12:00:52 -0700

    remove pyqt5 from Pipfile.lock

---
+++ README.md
@@ -0,0 +1,78 @@
+# Watermark App
+
+The Watermark App is a Python application that allows you to add watermarks to images. You can choose an image, specify a watermark text, and optionally add a logo to the image. The app provides a preview of the watermarked image and allows you to save the modified image with the watermark.
+![watermarking_desktop_app.png](watermarking_desktop_app.png)
+## Features
+
+- Select an image: Browse and choose an image file from your local system.
+- Select a logo: Browse and choose a logo image file to add as a watermark.
+- Watermark text: Enter the desired text for the watermark.
+- Update preview: Generate a preview of the watermarked image based on the selected image, watermark text, and logo.
+- Save watermarked image: Save the watermarked image with the applied watermark and logo to a desired location on your system.
+
+## Prerequisites
+
+- Python 3.x
+- PIL (Python Imaging Library)
+- Tkinter
+
+## Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/your_username/watermark-app.git
+   ```
+
+2. Change to the project directory:
+
+   ```bash
+   cd watermark-app
+   ```
+
+3. Install the required dependencies:
+
+   ```bash
+   pip install Pillow
+   ```
+
+## Usage
+
+1. Run the script in a Python environment:
+
+   ```bash
+   python watermark_app.py
+   ```
+
+2. The Watermark App window will appear.
+3. Click on the "Select Image" button to choose an image file.
+4. Click on the "Select Logo" button to choose a logo image file (optional).
+5. Enter the desired text for the watermark in the "Watermark Text" field.
+6. Click on the "Update Text" button to apply the entered watermark text.
+7. The preview of the watermarked image will be displayed in a separate window.
+8. Click on the "Update Preview" button to generate a preview of the watermarked image.
+9. Adjust the watermark text and logo as needed.
+10. Once satisfied with the preview, click on the "Save Watermarked Image" button to save the modified image with the watermark to a desired location.
+11. Choose the save location and provide a filename for the watermarked image.
+12. The watermarked image will be saved successfully, and a confirmation message will be displayed.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+## Acknowledgements
+
+The Watermark App utilizes the following libraries:
+
+- PIL (Python Imaging Library) - https://python-pillow.org/
+- Tkinter - Python's standard GUI package
+
+## Contributing
+
+Contributions are welcome! If you have any ideas, suggestions, or improvements, please open an issue or a pull request.
+
+## Contact
+
+For any questions or inquiries, please contact [your_email@example.com].
+
+Happy watermarking!
--- watermarking_desktop_app.py
+++ watermarking_desktop_app.py
@@ -1,32 +1,119 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QFileDialog
-from PyQt5.QtGui import QPixmap, QImage, QPainter, QFont, QIcon
-from PyQt5.QtCore import Qt
+from PIL import Image, ImageDraw, ImageFont, ImageTk
+from tkinter import filedialog, Tk, Button, Entry, Label, StringVar, Toplevel
 
 
-class WatermarkingApp(QMainWindow):
+class WatermarkApp:
     def __init__(self):
-        super().__init__()
+        self.image_path = None
+        self.image = None
+        self.text = None
+        self.logo_path = None
+        self.preview_image = None
+        self.preview_label = None
 
-        self.setWindowTitle("Watermarking App")
-        self.setGeometry(200, 200, 800, 600)
+    def select_image(self):
+        self.image_path = filedialog.askopenfilename()
 
-        self.image_label = QLabel(self)
-        self.image_label.setGeometry(20, 20, 400, 400)
-        self.image_label.setAlignment(Qt.AlignCenter)
+    def select_logo(self):
+        self.logo_path = filedialog.askopenfilename()
 
-        self.watermark_label = QPushButton("Add Text Watermark", self)
-        self.watermark_label.setGeometry(450, 50, 200, 30)
-        self.watermark_label.clicked.connect(self.open_watermark)
+    def update_preview(self):
+        if self.image_path:
+            self.preview_image = Image.open(self.image_path).convert("RGBA")
+            width, height = self.preview_image.size
+            draw = ImageDraw.Draw(self.preview_image)
+            font_size = round(self.preview_image.width * 0.05)
+            font = ImageFont.truetype("arial.ttf", font_size)
+            if self.text:
+                bbox = draw.textbbox((0, 0, width, height), self.text, font)
+                text_width = bbox[2] - bbox[0]
+                text_height = bbox[3] - bbox[1]
+                x = self.preview_image.width - text_width - round(self.preview_image.width * 0.05)
+                y = self.preview_image.height - text_height - round(self.preview_image.height * 0.05)
+                draw.text((x, y), self.text, font=font, fill=(255, 255, 255, 128))
+            if self.logo_path:
+                logo = Image.open(self.logo_path).convert("RGBA")
+                logo_size = round(min(self.preview_image.width, self.preview_image.height) * 0.2)
+                logo.thumbnail((logo_size, logo_size))
+                x = round(self.preview_image.width * 0.05)
+                y = round(self.preview_image.height * 0.05)
+                self.preview_image.paste(logo, (x, y), logo)
+            self.update_preview_label()
 
-        self.watermark_logo = QPushButton("Add Watermark Logo", self)
-        self.watermark_logo.setGeometry(450, 100, 200, 30)
-        self.watermark_logo.clicked.connect(self.open_watermark_logo)
+    def update_preview_label(self):
+        if self.preview_image:
+            photo = ImageTk.PhotoImage(self.preview_image)
+            self.preview_label.configure(image=photo)
+            self.preview_label.image = photo
 
-        self.show()
+    def save_watermarked_image(self):
+        if self.image_path:
+            image = Image.open(self.image_path).convert("RGBA")
+            width, height = image.size
+            draw = ImageDraw.Draw(image)
+            font_size = round(image.width * 0.05)
+            font = ImageFont.truetype("arial.ttf", font_size)
+            if self.text:
+                bbox = draw.textbbox((0, 0, width, height), self.text, font)
+                text_width = bbox[2] - bbox[0]
+                text_height = bbox[3] - bbox[1]
+                x = image.width - text_width - round(image.width * 0.05)
+                y = image.height - text_height - round(image.height * 0.05)
+                draw.text((x, y), self.text, font=font, fill=(255, 255, 255, 128))
+            if self.logo_path:
+                logo = Image.open(self.logo_path).convert("RGBA")
+                logo_size = round(min(image.width, image.height) * 0.2)
+                logo.thumbnail((logo_size, logo_size))
+                x = round(image.width * 0.05)
+                y = round(image.height * 0.05)
+                image.paste(logo, (x, y), logo)
+            save_path = filedialog.asksaveasfilename(defaultextension=".png")
+            if save_path:
+                image.save(save_path)
+                print("Watermark added successfully!")
 
-    def open_watermark(self):
-        pass
+    def create_gui(self):
+        root = Tk()
+        root.title("Watermark App")
 
-    def open_watermark_logo(self):
-        pass
+        select_image_button = Button(root, text="Select Image", command=self.select_image)
+        select_image_button.pack()
+
+        select_logo_button = Button(root, text="Select Logo", command=self.select_logo)
+        select_logo_button.pack()
+
+        text_entry_label = Label(root, text="Watermark Text:")
+        text_entry_label.pack()
+        text_entry = Entry(root, textvariable=StringVar())
+        text_entry.pack()
+
+        def update_text():
+            self.text = text_entry.get()
+
+        update_text_button = Button(root, text="Update Text", command=update_text)
+        update_text_button.pack()
+
+        preview_window = Toplevel(root)
+        preview_window.title("Preview")
+        self.preview_label = Label(preview_window)
+        self.preview_label.pack()
+
+        update_preview_button = Button(root, text="Update Preview", command=self.update_preview)
+        update_preview_button.pack()
+
+        save_button = Button(root, text="Save Watermarked Image", command=self.save_watermarked_image)
+        save_button.pack()
+
+        root.mainloop()
+
+    def run(self):
+        self.create_gui()
+
+
+if __name__ == "__main__":
+    watermark_app = WatermarkApp()
+    watermark_app.run()
+
+
+
+
